f8e2779c47c62aa10b97b87f239b2026786effdd
Madison Wyrick <0974563@my.scccd.edu>
2024-08-22 23:38:15 -0700

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,24 @@
-console.log('Hello World!')
+ const myHeading = document.querySelector("h2");
+ myHeading.textContent = "75!";
+
+//Carbon Footprint Method 
+
+ // 1. Count the members of your household.
+ const myHousehold = 8;
+ // 2. Consider the size of your home.
+ const myHousesize = 7;
+ // 3. Evaluate your food choices.
+ const myFoodchoices = 8;
+ // 4. Examine your water consumption.
+ const myWaterhabit = 2;
+ // 5. Determine how many household purchases you make in a year.
+ const myPurchases = 6;
+ // 6. How much waste do you produce?
+ const myWaste = 30;
+ // 7. Identify how much do you use recycle.
+ const myRecycling = 4;
+ // 8. Tally up your annual transportation scores.
+ const myTraveling = 10;
+ // 9. Add up your points.
+ const myTotal = 75;
+ // 10. Write JS to update the rendered html.
