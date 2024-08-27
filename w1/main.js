3a974be0cd88786cafda0e94e2ff4bef6d245957
Madison Wyrick <0974563@my.scccd.edu>
2024-08-26 20:05:03 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+console.log("hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "madison";
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType);
+console.log(`myVarType" ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will 14 this line run?`)
+    } else {
+        console.log(`will 16 this line will run`)
+    }    
+}
+
+runNow();
+runNow();
