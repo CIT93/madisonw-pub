035d5feb54bd981bae9a4482f1ab9a0fc15f7c68
Madison Wyrick <0974563@my.scccd.edu>
2024-09-30 22:23:18 -0700

    setup

--- w7/render.js
+++ w7/render.js
@@ -2,10 +2,12 @@ const TBL =  document.getElementById("tab-data")
 
 
 function renderTblHeading(data){
+    TBL.innerHTML = ""
     const table = document.createElement("table")
     const thead = document.createElement("thead")
     const tr = document.createElement("tr")
-    const headingTextArr = ["Name","HouseHold","HouseSize","Footprint","Actions"]
+    //const headingTextArr = ["Name","HouseHold","HouseSize","Footprint","Actions"]
+    const headingTextArr = ["Name","Footprint"]
     headingTextArr.forEach(function(text){
       const th = document.createElement("th");
       th.textContent = text
@@ -21,35 +23,29 @@ function renderTblHeading(data){
   function renderTbl(data) {
     const table = renderTblHeading()
     const tbody = document.createElement("tbody")
-    const tr = document.createElement("tr")
-    const trTextArr = [fname,houseM,houseS,cfptotal]
-    trTextArr.forEach(function(text){
-      const td = document.createElement("td")
-      td.textContent = text
-      tr.appendChild(td)
+    
+    //const trTextArr = [fname,houseM,houseS,cfptotal]
+    data.forEach(function(obj){
+      const tr = document.createElement("tr")
+      const tdName = document.createElement("td")
+      tdName.textContent = obj.fname
+      const tdTotal = document.createElement("td")
+      tdTotal.textContent = obj.cfpTotal
+      
+      tr.appendChild(tdName)
+      tr.appendChild(tdTotal)
+      tbody.appendChild(tr)
     })
-    const td = document.createElement("td")
-    const btnEdit = document.createElement("button")
-    const btnDel = document.createElement("button")
-    btnEdit.textContent = "Edit"
-    btnDel.textContent = "Del"
-    td.appendChild(btnEdit)
-    td.appendChild(btnDel)
-    tr.appendChild(td)
-    tbody.appendChild(tr)
+    //const td = document.createElement("td")
+    //const btnEdit = document.createElement("button")
+    //const btnDel = document.createElement("button")
+    //btnEdit.textContent = "Edit"
+    //btnDel.textContent = "Del"
+    //td.appendChild(btnEdit)
+    //td.appendChild(btnDel)
+    //tr.appendChild(td)
+    //tbody.appendChild(tr)
     table.appendChild(tbody)
-    console.log(table)
-    cfpData.push( {
-      houseM: houseHoldMembers,
-      houseS: houseSize,
-      houseMPTS: houseHoldPTS,
-      houseSPTS: houseSizePTS,
-      cfpTotal: total,
-      fname: firstName,
-      lname: lastName
-    } )
-  
-  
     TBL.appendChild(table)
    
   }
