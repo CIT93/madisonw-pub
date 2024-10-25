08e0a120bd45e2915c1bd4b2d2148c313fba87d1
Madison Wyrick <0974563@my.scccd.edu>
2024-10-24 21:02:48 -0700

    code before solution

---
+++ w11/global.js
@@ -0,0 +1,11 @@
+export const FORM = document.getElementById("form")
+
+export const TBL =  document.getElementById("tab-data")
+
+export const FNAMEEL = document.getElementById('firstName')
+
+export const LNAMEEL = document.getElementById('lastName')
+
+export const SUBMIT = document.getElementById("submitError")
+
+
